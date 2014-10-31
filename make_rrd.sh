692a7636b0a782e68b84a7a2032a3bf96e4d6ef3
AnotherVisitor <heiko.wagner@gmail.com>
2014-10-31 20:05:56 +0100

    Create make_rrd.sh

---
+++ make_rrd.sh
@@ -0,0 +1,8 @@
+rrdtool create weather.rrd --step 900 \
+DS:tempin:GAUGE:1200:-40:80 \
+DS:tempout:GAUGE:1200:-40:80 \
+DS:pnn:GAUGE:1200:950:1050 \
+RRA:AVERAGE:0.5:1:960 \
+RRA:MIN:0.5:96:3600 \
+RRA:MAX:0.5:96:3600 \
+RRA:AVERAGE:0.5:96:3600
